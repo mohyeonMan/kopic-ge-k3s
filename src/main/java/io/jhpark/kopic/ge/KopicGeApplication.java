63c54fcffc444b950c623f376a56b2073f251b04
mohyeonMan <qkrwlgns0510@gmail.com>
2026-04-15 16:23:32 +0900

    init : first commit

---
+++ src/main/java/io/jhpark/kopic/ge/KopicGeApplication.java
@@ -0,0 +1,12 @@
+package io.jhpark.kopic.ge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopicGeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopicGeApplication.class, args);
+	}
+}
---
+++ src/test/java/io/jhpark/kopic/ge/KopicGeApplicationTests.java
@@ -0,0 +1,12 @@
+package io.jhpark.kopic.ge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopicGeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
